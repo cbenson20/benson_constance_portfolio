733ed4ece9cfa6920c8051b66ea8cafd52a6d05c
cbenson20 <cbenson2@academic.rrc.ca>
2025-11-17 12:29:57 -0600

    Added a skills.ts file

---
+++ src/portfolioData/skills.ts
@@ -0,0 +1,63 @@
+export const skills = {
+  programmingLanguages: [
+    "C#",
+    "Java",
+    "JavaScript (React.js, TypeScript)",
+    "Python",
+    "HTML5",
+    "CSS3",
+  ],
+
+  frameworksAndTools: [
+    "React",
+    ".NET Core",
+    "Node.js",
+    "Docker",
+    "GitLab CI/CD",
+    "Jenkins",
+    "GitHub Actions",
+    "Storybook",
+    "Styled Components",
+    "Jest",
+    "ESLint",
+    "Prettier",
+    "Rails",
+    "Bootstrap",
+  ],
+
+  cloudPlatforms: ["AWS", "Azure", "Vercel", "Nginx"],
+
+  apisAndIntegration: [
+    "REST",
+    "SOAP",
+    "GraphQL",
+    "JSON",
+    "XML",
+    "Postman",
+    "Insomnia",
+  ],
+
+  architecture: [
+    "Microservices",
+    "Layered",
+    "MVC",
+    "Clean Architecture",
+    "Design Patterns",
+  ],
+
+  testing: ["Unit Testing", "Integration Testing", "Jest", "Mocha"],
+
+  automationAndRPA: [
+    "Microsoft Power Automate",
+    "Power Apps",
+    "Python (Pandas, Selenium)",
+  ],
+
+  collaborationAndDocs: [
+    "Microsoft 365",
+    "Visio",
+    "Lucidchart",
+    "Markdown",
+    "Agile / Scrum",
+  ],
+};
