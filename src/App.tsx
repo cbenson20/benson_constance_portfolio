1412b5d7308ecf5ae9a576d03fad1e67dcb94003
cbenson20 <cbenson2@academic.rrc.ca>
2025-11-22 21:06:13 -0600

    testing for Github Action

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import React from "react";
 import styled from "styled-components";
 import { Button } from "./components/Button";
-import { Label } from "./components/Label";
-import { Text } from "./components/Text";
-import { Table, TableHeader, TableRow, TableCell, TableFooter } from "./components/Table";
-import { Dropdown } from "./components/Dropdown";
-import { RadioButton } from "./components/RadioButton";
-import { Img } from "./components/Img";
-import { HeroImage } from "./components/HeroImage";
-import { Card } from "./components/Card";
 import { BasicInfoSection } from "./sections/BasicInfoSection";
 import { ProjectsSection } from "./sections/ProjectsSection";
 import { SkillsSection } from "./sections/SkillsSection";
